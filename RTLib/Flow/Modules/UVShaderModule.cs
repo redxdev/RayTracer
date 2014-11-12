36c7ca4d0270cccd71828d2b402f625918248856
Sam Bloomberg <sam@redxdev.com>
2014-11-12 16:22:13 -0500

    fixed uv coords on sphere

---
+++ RTLib/Flow/Modules/UVShaderModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Complex;
+using RTLib.Material;
+using RTLib.Util;
+
+namespace RTLib.Flow.Modules
+{
+    [Module]
+    public class UVShaderModule : IModuleBuilder
+    {
+        public string GetModuleName()
+        {
+            return "Shader.UV";
+        }
+
+        public IFlowValue CreateModule(FlowScene scene, IDictionary<string, IFlowValue> parameters)
+        {
+            UVShader shader = new UVShader();
+            return new GenericValue<UVShader>() { Value = shader };
+        }
+    }
+}
